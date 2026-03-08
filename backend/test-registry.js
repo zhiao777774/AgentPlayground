dd325f265c38cd2aeca9a3764d230c40c81d30a7
Mark Hsu <zhiaohsu@gmail.com>
2026-03-08 23:19:43 +0800

    Untrack backend/test-* files

--- backend/test-registry.js
+++
@@ -1,6 +0,0 @@
-import { ModelRegistry } from '@mariozechner/pi-coding-agent';
-
-const registry = new ModelRegistry();
-const all = registry.getAll();
-const ollama = all.filter(m => m.id.includes('qwen'));
-console.log(ollama);
